edbcd8da1bfb8a33e9db76cba6f6210fb560ce48
beybora <bora.b.sarikaya@gmail.com>
2025-01-25 17:56:23 +0100

    feat: add Cover component with background image support

---
+++ components/Cover/Cover.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import React from "react";
+
+const Cover = ({ children, background }) => {
+  return (
+    <div className="h-screen text-black bg-red-100 bg-cover relative min-h-[400px] flex items-center justify-center">
+      <Image
+        src={background}
+        alt="cover"
+        fill
+        className="object-cover mix-blend-soft-light"
+      />
+      <div className="max-w-5xl z-10">{children}</div>
+    </div>
+  );
+};
+
+export default Cover;
