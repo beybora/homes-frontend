bf12c0dadba458ed9340fce9be11c08752fd28f8
beybora <bora.b.sarikaya@gmail.com>
2025-01-25 17:34:11 +0100

    feat: add font size utility function

--- utils/fonts.js
+++ utils/fonts.js
@@ -8,3 +8,15 @@ export const getTextAlign = (textAlign = "left") => {
     return `${textAlignMap[textAlign] || "text-left"}`;
 };
      
+export const getFontSize = (level) => {
+    const fontSizeMap = {
+        1: "text-6xl",
+        2: "text-5xl",
+        3: "text-4xl",
+        4: "text-3xl",
+        5: "text-2xl",
+        6: "text-xl",
+    }
+
+    return `${fontSizeMap[level] || "text-base"}` ;
+}
