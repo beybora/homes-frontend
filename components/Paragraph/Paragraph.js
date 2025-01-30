2c4580e17d558a58468d45d1729554c096a66532
beybora <bora.b.sarikaya@gmail.com>
2025-01-30 21:44:00 +0100

    feat: add URL transformation to Paragraph content

--- components/Paragraph/Paragraph.js
+++ components/Paragraph/Paragraph.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { getTextAlign } from "utils/fonts";
+import { relativeToAbsoluteUrls } from "utils/relativeToAbsoluteUrls";
 
 export const Paragraph = ({ content, textAlign }) => {
   return (
     <p
       className={`max-w-5xl mx-auto ${getTextAlign(textAlign)}`}
-      dangerouslySetInnerHTML={{ __html: content }}
+      dangerouslySetInnerHTML={{ __html: relativeToAbsoluteUrls(content) }}
     />
   );
 };
