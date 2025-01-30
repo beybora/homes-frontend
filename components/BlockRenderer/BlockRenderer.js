1d2befa3bbec0430ed1322a5c6030e36aaca9167
beybora <bora.b.sarikaya@gmail.com>
2025-01-30 21:45:50 +0100

    feat: extend BlockRenderer with core block components support

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,8 +1,34 @@
+import Cover from "components/Cover/Cover";
+import Heading from "components/Heading/Heading";
+import Paragraph from "components/Paragraph/Paragraph";
+
 export const BlockRenderer = ({ blocks }) => {
-  return blocks.map((block, index) => {
+  return blocks.map((block) => {
     switch (block.name) {
+      case "core/heading":
+        return (
+          <Heading
+            key={block.id}
+            content={block.attributes.content}
+            level={block.attributes.level}
+            textAlign={block.attributes.textAlign}
+          />
+        );
       case "core/cover":
-        return <div key={block.id}>Cover </div>;
+        return (
+          <Cover key={block.id} background={block.attributes.url}>
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Cover>
+        );
+      case "core/paragraph":
+        return (
+          <Paragraph
+            key={block.id}
+            content={block.attributes.content}
+            textAlign={block.attributes.textAlign}
+
+          />
+        );
       default:
         return null;
     }
