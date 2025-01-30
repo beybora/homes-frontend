25ec702e418e7cca1c6a0cd96f9b0b9f3eacaad9
beybora <bora.b.sarikaya@gmail.com>
2025-01-30 22:40:31 +0100

    feat: create ButtonLink component

---
+++ components/ButtonLink/ButtonLink.js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import React from "react";
+
+const ButtonLink = ({ destination, label }) => {
+  return (
+    <Link
+      href={destination}
+      className="bg-pink-300 px-4 py-2 rounded-md inline-block my-4 uppercase cursor-pointer font-bold text-white"
+    >
+      {label}
+    </Link>
+  );
+};
+
+export default ButtonLink;
