ee965f211858642e3204e208b11f43c79bdcacf8
beybora <bora.b.sarikaya@gmail.com>
2025-01-30 21:41:24 +0100

    feat: add Main Menu component

---
+++ components/MainMenu/MainMenu.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const MainMenu = ({ items }) => {
+  console.log(items, "main menu");
+  return (
+    <div className="bg-slate-800 text-white px-5 h-16 sticky top-0 z-50 flex justify-between items-center">
+      
+    </div>
+  );
+};
