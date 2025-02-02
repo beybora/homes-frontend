2c9ff18e208bbe4e8694fe60ae2c9fff35e7475e
beybora <bora.b.sarikaya@gmail.com>
2025-02-02 20:12:36 +0100

    style: Update ButtonLink component to use Tailwind CSS class

--- components/ButtonLink/ButtonLink.js
+++ components/ButtonLink/ButtonLink.js
@@ -5,7 +5,7 @@ const ButtonLink = ({ destination, label }) => {
   return (
     <Link
       href={destination}
-      className="bg-pink-300 px-4 py-2 rounded-md inline-block uppercase cursor-pointer font-bold text-white"
+      className="btn"
     >
       {label}
     </Link>
