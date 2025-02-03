1faa220129877238bd50875905a2c931f100f841
beybora <bora.b.sarikaya@gmail.com>
2025-02-03 18:13:07 +0100

    feat: add Columns to Blockrenderer

--- components/BlockRenderer/BlockRenderer.js
+++ components/BlockRenderer/BlockRenderer.js
@@ -1,8 +1,12 @@
+import Column from "components/Column/Column";
+import Columns from "components/Columns/Columns";
 import Cover from "components/Cover/Cover";
 import Heading from "components/Heading/Heading";
 import Paragraph from "components/Paragraph/Paragraph";
+import Image from "next/image";
 
 export const BlockRenderer = ({ blocks }) => {
+  console.log(blocks);
   return blocks.map((block) => {
     switch (block.name) {
       case "core/heading":
@@ -26,7 +30,34 @@ export const BlockRenderer = ({ blocks }) => {
             key={block.id}
             content={block.attributes.content}
             textAlign={block.attributes.textAlign}
-
+          />
+        );
+      case "core/columns":
+        return (
+          <Columns
+            key={block.id}
+            isStackedOnMobile={block.attributes.isStackedOnMobile}
+          >
+            <BlockRenderer blocks={block.innerBlocks} />
+          </Columns>
+        );
+      case "core/column":
+        return (
+          <Column key={block.id}>
+            {" "}
+            <BlockRenderer
+              blocks={block.innerBlocks}
+            />
+          </Column>
+        );
+      case "core/image":
+        return (
+          <Image
+            key={block.id}
+            src={block.attributes.url}
+            height={block.attributes.height}
+            width={block.attributes.width}
+            alt={block.attributes.alt || ""}
           />
         );
       default:
