416882482e3240dee087ee7ef84a048f7c7248d1
beybora <bora.b.sarikaya@gmail.com>
2025-02-05 22:55:12 +0100

    feat: create PropertySearch/ Results Component

---
+++ components/PropertySearch/Results/Results.js
@@ -0,0 +1,25 @@
+import PropertyCard from "./PropertyCard/PropertyCard";
+
+const Results = ({ properties }) => {
+  return (
+    <div className="max-w-5xl mx-auto grid grid-cols-3 gap-5 mb-10 mt-10">
+      {console.log(properties)}
+      {properties.map((property) => (
+        <PropertyCard 
+          key={property.databaseId}
+          title={property.title}
+          destination={property.uri}
+          bedrooms={property.propertyFeatures.bedrooms}
+          bathrooms={property.propertyFeatures.bathrooms}
+          price={property.propertyFeatures.price}
+          hasParking={property.propertyFeatures.hasParking}
+          petFriendly={property.propertyFeatures.petFriendly}
+          image={property.featuredImage?.node?.sourceUrl}
+          uri={property.uri}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Results; 
