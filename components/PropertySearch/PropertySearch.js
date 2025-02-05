a3010e0f552093e8a01907cdf354a0cddbdce187
beybora <bora.b.sarikaya@gmail.com>
2025-02-05 22:54:52 +0100

    feat: add Property Search Component

---
+++ components/PropertySearch/PropertySearch.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import Results from "./Results/Results";
+
+const PropertySearch = () => {
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const response = await fetch("/api/search");
+      const data = await response.json();
+      setProperties(data.properties);
+    };
+    search();
+  }, []);
+
+  return (
+    <div>
+      <Results properties={properties} />
+    </div>
+  );
+};
+
+export default PropertySearch;
