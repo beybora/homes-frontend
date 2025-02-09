737c51cfa54398bc2cbc491188fe4504d7eeaca4
beybora <bora.b.sarikaya@gmail.com>
2025-02-09 18:00:14 +0100

    feat: implement SEO meta tags in Page component

--- components/Page/Page.js
+++ components/Page/Page.js
@@ -1,14 +1,20 @@
 import { BlockRenderer } from 'components/BlockRenderer'
 import { MainMenu } from 'components/MainMenu'
+import Head from 'next/head';
 import React from 'react'
 
-const Page = ({ blocks, mainMenuItems }) => {
+const Page = ({ blocks, mainMenuItems, seo }) => {
+  console.log(seo, "seo");
   return (
     <div>
+      <Head>
+        <title>{seo.title}</title>
+        <meta name="description" content={seo.metaDesc} />
+      </Head>
       <MainMenu items={mainMenuItems} />
       <BlockRenderer blocks={blocks} />
     </div>
-  )
+  );
 }
 
 export default Page
